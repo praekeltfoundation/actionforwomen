f0d4e85340f291c7e5830484407648dd8a305091
Shaun Sephton <shaun@28lines.com>
2012-05-11 16:04:52 +0200

    view modifier for popular ordering

--- mama/views.py
+++ mama/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from mama.view_modifiers import PopularViewModifier
 from post.models import Post
 
 
@@ -34,6 +35,8 @@ class CategoryListView(ListView):
     def get_queryset(self):
         self.category = get_object_or_404(Category, \
                 slug__iexact=self.kwargs['category_slug'])
-        return Post.permitted.filter(
+        queryset = Post.permitted.filter(
             Q(primary_category=self.category) | Q(categories=self.category)
         )
+        view_modifier = PopularViewModifier(self.request)
+        return view_modifier.modify(queryset)
