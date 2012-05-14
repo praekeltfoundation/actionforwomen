937dfd19844d0e7c9022eb01ee9c274f06ffcdab
Shaun Sephton <shaun@28lines.com>
2012-05-14 12:26:47 +0200

    first commit

---
+++ mama/search_indexes.py
@@ -0,0 +1,13 @@
+from haystack.indexes import *
+from haystack import site
+from post.models import Post
+
+
+class PostIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+
+    def index_queryset(self):
+        return Post.permitted.all()
+
+
+site.register(Post, PostIndex)
