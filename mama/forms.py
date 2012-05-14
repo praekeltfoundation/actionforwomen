a1e44a49a047050e898a9627e0e752e854f4b2a1
Shaun Sephton <shaun@28lines.com>
2012-05-14 08:43:15 +0200

    enforce require for profile fields

--- mama/forms.py
+++ mama/forms.py
@@ -5,6 +5,10 @@ from registration.forms import RegistrationFormTermsOfService
 from userprofile import utils
 
 
+class PasswordResetForm(forms.Form):
+    mobile_number = forms.CharField(max_length=64)
+
+
 class RegistrationForm(RegistrationFormTermsOfService):
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
@@ -23,10 +27,9 @@ class RegistrationForm(RegistrationFormTermsOfService):
             'tos',
         ]
         self.fields['email'].label = 'Email address'
+        self.fields['mobile_number'].required = True
+        self.fields['weeks_pregnant_signup'].required = True
         self.fields['weeks_pregnant_signup'].label = 'How long have you been pregnant?'
         self.fields['weeks_pregnant_signup'].choices = [('', 'Select the number of weeks')] + self.fields['weeks_pregnant_signup'].choices[1:]
         self.fields['password2'].label = 'Confirm your password'
         self.fields['tos'].label = mark_safe('I accept the <a href="%s">terms and conditions</a> of use.' % reverse("terms"))
-
-class PasswordResetForm(forms.Form):
-    mobile_number = forms.CharField(max_length=64)
