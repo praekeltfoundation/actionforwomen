26ce0738581148fe915e2d8ba189bcb2466452d4
Shaun Sephton <shaun@28lines.com>
2012-05-16 11:35:29 +0200

    revert back to listing view with featured footer

--- mama/views.py
+++ mama/views.py
@@ -53,21 +53,11 @@ class CategoryListView(ListView):
         return view_modifier.modify(queryset)
 
 
-class CategoryListFeaturedView(ListView):
+class CategoryListFeaturedView(CategoryListView):
     template_name = "post/post_category_list_featured.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(CategoryListFeaturedView, self).get_context_data(**kwargs)
-        context['category'] = self.category
-        return context
-
     def get_queryset(self):
-        self.category = get_object_or_404(Category, \
-                slug__iexact=self.kwargs['category_slug'])
-        queryset = Post.permitted.filter(
-            Q(primary_category=self.category) | Q(categories=self.category)
-        ).filter(categories__slug='featured').distinct()
-        return queryset
+        return super(CategoryListFeaturedView, self).get_queryset().exclude(categories__slug='featured')
 
 
 class ContactView(FormView):
