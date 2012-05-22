2fc2741f43df05358f9a3bfc238828fecafb64a5
Shaun Sephton <shaun@28lines.com>
2012-05-22 12:28:47 +0200

    implemented throllted sms reset

--- mama/forms.py
+++ mama/forms.py
@@ -1,6 +1,14 @@
+import ambient
+from datetime import date
 from django import forms
+from django.conf import settings
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.models import get_current_site
 from django.core.urlresolvers import reverse
+from django.utils.http import int_to_base36
 from django.utils.safestring import mark_safe
+import mama
 from registration.forms import RegistrationFormTermsOfService
 from userprofile import utils
 
@@ -13,8 +21,54 @@ class ContactForm(forms.Form):
     )
 
 
-class PasswordResetForm(forms.Form):
+class PasswordResetForm(PasswordResetForm):
     mobile_number = forms.CharField(max_length=64)
+   
+    def __init__(self, *args, **kwargs):
+        super(PasswordResetForm, self).__init__(*args, **kwargs)
+        del self.fields['email']
+
+    def clean_mobile_number(self):
+        """
+        Validates that an active user exists with the given email address.
+        """
+        mobile_number = self.cleaned_data['mobile_number']
+        # Fail with invalid number.
+        try:
+            self.profile = mama.models.UserProfile.objects.get(mobile_number__exact=mobile_number)
+            self.user = self.profile.user
+        except mama.models.UserProfile.DoesNotExist:
+            raise forms.ValidationError("Unable to find an account for the provided mobile number. Please try again.")
+        # Fail if user has already reset password today more than once.
+        if self.profile.last_reset_date == date.today() and self.profile.reset_count >= 2:
+            raise forms.ValidationError("You have already tried to reset your password today. Please wait for your SMS or try again tomorrow.")
+        return mobile_number
+    
+    def save(self, *args, **kwargs):
+        """
+        Generates a one-use only link for resetting password and sends to the
+        user.
+        """
+        # Before we do anything else update reset counts for throttling.
+        if self.profile.last_reset_date == date.today():
+            self.profile.reset_count += 1
+        else:
+            self.profile.last_reset_date = date.today()
+            self.profile.reset_count = 1
+        self.profile.save()
+
+        # Generate message containing url with users token.
+        uid = int_to_base36(self.user.id)
+        token = default_token_generator.make_token(self.profile.user)
+        current_site = get_current_site(kwargs['request'])
+        message = "Follow this link to reset your %s password: http://%s%s" % (current_site.name, current_site.domain, reverse('password_reset_confirm', kwargs={'uidb36': uid, 'token': token}))
+
+        # Send the message using Ambient's gateway.
+        sms = ambient.AmbientSMS(
+            settings.AMBIENT_API_KEY,
+            settings.AMBIENT_GATEWAY_PASSWORD
+        )
+        sms.sendmsg(message, [self.profile.mobile_number, ])
 
 
 class RegistrationForm(RegistrationFormTermsOfService):
