6fdd0f8c43b63ebd1825007b295d8c270fe20773
Shaun Sephton <shaun@28lines.com>
2012-08-16 17:06:55 +0200

    google-credentials install and settings

--- mama/settings.py
+++ mama/settings.py
@@ -144,6 +144,7 @@ INSTALLED_APPS = (
     'category',
     'ckeditor',
     'generate',
+    'google_credentials',
     'haystack',
     'jmbo',
     'moderator',
@@ -226,3 +227,8 @@ SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
 # Set session cookie age to 1 year, meaning sessions are valid for up to 1 year.
 SESSION_COOKIE_AGE = 31536000
+
+GA_CLIENT_ID = '938739567758.apps.googleusercontent.com'
+GA_CLIENT_SECRET = 'bpiXTbuh95MI8DQlDCIr02kf'
+GA_SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+GA_REDIRECT_URI = 'http://askmama.mobi/google-credentials/callback'
