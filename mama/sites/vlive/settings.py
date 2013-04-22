0a004b7c80bd7a52ae667c501987e0da31ebc3ff
Shaun Sephton <connect@shaunsephton.com>
2013-04-22 19:25:03 +0200

    install pml app

--- mama/sites/vlive/settings.py
+++ mama/sites/vlive/settings.py
@@ -4,6 +4,10 @@ TEMPLATE_DIRS = (
     os.path.join(PATH, "mama", "templates", "vlive"),
 )
 
+INSTALLED_APPS += (
+    'pml',
+)
+
 MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
     'mama.middleware.PMLMiddleware',
     'mama.middleware.PMLFormMiddleware',
