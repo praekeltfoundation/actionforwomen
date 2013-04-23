9d6fad54b5f33682be7c76c1ff20f186afae9420
Shaun Sephton <connect@shaunsephton.com>
2013-04-23 09:29:12 +0200

    dont' set content type header to text/xml for admin or error responses

--- mama/middleware.py
+++ mama/middleware.py
@@ -1,8 +1,21 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+
 class PMLMiddleware(object):
     """
     Sets response mimetype to text/xml.
     """
     def process_response(self, request, response):
+        # When developing on error show trace page
+        if settings.DEBUG and response.status_code in [500, ]:
+            return response
+
+        # Don't apply xml to admin views.
+        if request.path.startswith(reverse('admin:index')):
+            return response
+
+        # Ste xml content type for everything else.
         response['Content-Type'] = 'text/xml'
         return response
 
