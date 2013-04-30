0b736a8ae829f52e0f1c47afa98e72a121a17456
Shaun Sephton <connect@shaunsephton.com>
2013-04-30 08:26:31 +0200

    pin category version

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     ],
     install_requires=[
         'django==1.4.5',
+        'django-category==0.1',
         'django-export',
         'django-generate',
         'django-google-credentials',
