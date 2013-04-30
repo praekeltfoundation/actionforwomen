1883a15ae0ec29e8d2bafc922d4372752c32abe1
Shaun Sephton <connect@shaunsephton.com>
2013-04-30 06:06:20 +0200

    update manage to new structure

--- mama/sites/mobi/manage.py
+++ mama/sites/mobi/manage.py
@@ -1,25 +1,10 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-import imp
-import logging
+import os
 import sys
-import traceback
-try:
-    imp.find_module('settings')  # Assumed to be in the same directory.
-except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the "\
-            "directory containing %r. It appears you've customized things.\n"\
-            "You'll have to run django-admin.py, passing it your settings "\
-            "module.\n" % __file__)
-    sys.exit(1)
 
-import settings
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mama.sites.mobi.settings")
 
-if __name__ == '__main__':
-    try:
-        execute_manager(settings)
-    except Exception, e:
-        # Log errors to Sentry.
-        exc_info = sys.exc_info()
-        logging.error(e, exc_info=exc_info)
-        traceback.print_exc()
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
