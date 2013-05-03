70210b59be41c1cd4ba224ea89b9262997edc0e0
Shaun Sephton <connect@shaunsephton.com>
2013-05-03 05:20:22 +0200

    "cleanup"

--- mama/settings_dev.py
+++ mama/settings_dev.py
@@ -1,17 +1,11 @@
 from mama.settings import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mama.sqlite',                      # Or path to database file if using sqlite3.
-    }
-}
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-INSTALLED_APPS += (
-    'snippetscream',
-)
-
-CREATE_DEFAULT_SUPERUSER = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mama.sqlite',
+    }
+}
