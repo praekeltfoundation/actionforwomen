59340a2143a0159bcacb62b6a300676c17784dd0
Jurgen Blignaut <jurgen@webtide.co.za>
2013-07-10 15:08:51 +0200

    Added new settings for survey app to settings.py

--- mama/sites/mobi/settings.py
+++ mama/sites/mobi/settings.py
@@ -2,6 +2,15 @@ from mama.settings import *
 
 TEMPLATE_DIRS += (
     os.path.join(PATH, "mama", "templates", "mobi"),
+    os.path.join(PATH, "survey", "templates", "mobi"),
 )
 
 CACHES['default']['KEY_PREFIX'] = 'mama_mobi'
+
+LOGIN_REDIRECT_URL = '/survey/check-for-survey/'
+
+INSTALLED_APPS += (
+    'survey',
+)
+
+HOLODECK_URL = 'http://holodeck.praekelt.com/'
