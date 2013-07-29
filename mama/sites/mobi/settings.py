c4cc36411a72d3b9aced93342d42889e5996ded4
Jurgen Blignaut <jurgen@webtide.co.za>
2013-07-29 11:09:07 +0200

    Implemented vlive templates for survey app

--- mama/sites/mobi/settings.py
+++ mama/sites/mobi/settings.py
@@ -2,7 +2,7 @@ from mama.settings import *
 
 TEMPLATE_DIRS += (
     os.path.join(PATH, "mama", "templates", "mobi"),
-    os.path.join(PATH, "ve", "src", "mama-survey", "survey", "templates", "mobi"),
+    # os.path.join(PATH, "ve", "src", "mama-survey", "survey", "templates", "mobi"),
 )
 
 CACHES['default']['KEY_PREFIX'] = 'mama_mobi'
