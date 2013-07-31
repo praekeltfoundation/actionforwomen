4b3262f8b71003175815f0294f42d31b4ef43cf8
Jurgen Blignaut <jurgen@webtide.co.za>
2013-07-31 18:24:36 +0200

    Use PMLHiddenField for hidden fields in the survey forms

--- mama/sites/vlive/survey_forms.py
+++ mama/sites/vlive/survey_forms.py
@@ -2,7 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 
 from pml.forms import PMLForm
-from pml.forms.fields import PMLRadioField
+from pml.forms.fields import PMLRadioField, PMLHiddenField
 
 from survey.forms import _SURVEY_CHOICES
 
@@ -10,7 +10,7 @@ from survey.forms import _SURVEY_CHOICES
 class PMLSurveyChoiceForm(PMLForm):
     """ Choose how to proceed with a survey on the VLIVE site
     """
-    survey_id = forms.Field(widget=forms.HiddenInput)
+    survey_id = PMLHiddenField()
     proceed_choice = PMLRadioField(
         choices=_SURVEY_CHOICES,
         label=_('Would you like to participate?'),
@@ -21,6 +21,6 @@ class PMLSurveyQuestionForm(PMLForm):
     """ Display the options and capture the answer for one question in the
         survey on the VLIVE site.
     """
-    survey_id = forms.Field(widget=forms.HiddenInput)
-    question_id = forms.Field(widget=forms.HiddenInput)
+    survey_id = PMLHiddenField()
+    question_id = PMLHiddenField()
     question = PMLRadioField()
