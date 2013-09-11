8934e7e807bebb98710b527ffcf2d3e35a3ef816
Jurgen Blignaut <jurgen@webtide.co.za>
2013-09-11 12:52:43 +0200

    Created improved inclusion tag for your stories competition

---
+++ mama/templatetags/moms_stories_inclusion_tags.py
@@ -0,0 +1,25 @@
+from copy import copy
+
+from django import template
+from django.db.models import Q
+from django.template import Context, Template
+from django.contrib.sites.models import Site
+
+from category.models import Category
+from jmboyourwords.models import YourStoryCompetition
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('mama/inclusion_tags/story_competition.html',
+                        takes_context=True)
+def your_story_competition(context):
+    """ Display the latest available your story competition.
+    """
+    context = copy(context)
+    qs = YourStoryCompetition.objects.filter(published=True)
+    qs = qs.filter(sites=Site.objects.get_current())
+    qs = qs.order_by('-publish_on')
+    context['your_story_competition'] = qs.latest('publish_on')
+    return context
