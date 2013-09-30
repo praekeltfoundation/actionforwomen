53aa610f6f39f67b2ad08660db6569d35189e18c
gitumarkk <gitumarkk@gmail.com>
2013-09-30 16:17:26 +0200

    created basic unittests for hotsocket_login

--- monitor/tests.py
+++ monitor/tests.py
@@ -1,16 +1,26 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from mock import patch, call
+import requests
+from monitor.tasks import hotsocket_login
+from django.test.utils import override_settings
+from django.conf import settings
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestHotSocketAPi(TestCase):
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS = True,
+                       CELERY_ALWAYS_EAGER = True,
+                       BROKER_BACKEND = 'memory',)
+
+    def test_login_to_API(self):
+        data = {
+                "username": settings.HOTSOCKET_USERNAME,
+                "password": settings.HOTSOCKET_PASSWORD,
+                "as_json": True
+            }
+        url = "%s%s" % (settings.HOTSOCKET_BASE, settings.HOTSOCKET_RESOURCES["login"])
+
+        with patch.object(requests, 'post') as mock_login:
+            hotsocket_login()
+            mock_login.assert_called_once_with(url, data=data)
+
+
