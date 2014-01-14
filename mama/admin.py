751ba7d79d97adae4f39e4e71aae382ad034d17d
Milton <madandat@gmail.com>
2014-01-14 14:45:51 +0200

    pep8 fixes

--- mama/admin.py
+++ mama/admin.py
@@ -3,7 +3,6 @@ from dateutil.relativedelta import *
 
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
-from django.contrib.contenttypes import generic
 from django.contrib.comments.models import Comment
 from django.contrib.admin.sites import NotRegistered
 from django.contrib.contenttypes.models import ContentType
@@ -16,18 +15,18 @@ from secretballot.models import Vote
 from post.models import Post
 from poll.models import Poll
 from poll.admin import PollAdmin
-from livechat.models import LiveChat
-from jmboyourwords.admin import YourStoryEntryAdmin, YourStoryCompetitionAdmin
-from jmboyourwords.models import YourStoryEntry, YourStoryCompetition
+from jmboyourwords.admin import YourStoryEntryAdmin
+from jmboyourwords.models import YourStoryEntry
 from category.models import Category
 from survey.models import ContentQuizToPost
 from mama.models import (
-    Link, 
-    NavigationLink, 
-    SitePreferences, 
+    Link,
+    NavigationLink,
+    SitePreferences,
     Banner,
     DefaultAvatar,
 )
+
 
 class MamaModelbaseAdmin(ModelBaseAdmin):
     raw_id_fields = ('owner', )
@@ -82,7 +81,7 @@ class DefaultAvatarAdmin(admin.ModelAdmin):
     def _image(self, obj):
         # todo: use correct photologue scale
         if obj.image:
-            return """<img src="%s" height="48" width="48" />""" % obj.image.url
+            return '<img src="%s" height="48" width="48" />' % obj.image.url
         return ""
     _image.short_description = 'Image'
     _image.allow_tags = True
@@ -135,7 +134,7 @@ class WeeklyFilter(admin.filters.SimpleListFilter):
                                            second=0, microsecond=0)
         end_thursday = NOW + relativedelta(weekday=TH(+1),
                                            hour=0, minute=0,
-                                           second=0, microsecond=0, 
+                                           second=0, microsecond=0,
                                            microseconds=-1)
         if end_thursday < start_friday:
             end_thursday += relativedelta(weeks=1)
@@ -147,11 +146,11 @@ class WeeklyFilter(admin.filters.SimpleListFilter):
 
         if weeks_ago < 2:
             # Filter the questions between the date range
-            queryset = queryset.filter(submit_date__range=(start_friday, 
+            queryset = queryset.filter(submit_date__range=(start_friday,
                                                            end_thursday,))
         else:
             # Filter all the older questions.
-            queryset = queryset.filter(submit_date__lt=(end_thursday)) 
+            queryset = queryset.filter(submit_date__lt=(end_thursday))
 
         return queryset
 
@@ -185,28 +184,27 @@ class AskMamaQuestionAdmin(CommentAdmin):
         questions = questions.exclude(user__is_staff=True)
 
         # Work out the vote count for the questions, to sort by the most liked
-        # questions, i.e. questions with the most votes. (This is taken from the
-        # MostLikedItem view modifier item in jmbo)
+        # questions, i.e. questions with the most votes. (This is taken from
+        # the  MostLikedItem view modifier item in jmbo)
         questions = questions.extra(
             select={
                 'vote_score': '(SELECT COUNT(*) from %s WHERE vote=1 AND \
     object_id=%s.%s AND content_type_id=%s) - (SELECT COUNT(*) from %s WHERE \
     vote=-1 AND object_id=%s.%s AND content_type_id=%s)' % (
-                    Vote._meta.db_table,
-                    Comment._meta.db_table,
-                    Comment._meta.pk.attname,
-                    ContentType.objects.get_for_model(Comment).id,
-                    Vote._meta.db_table,
-                    Comment._meta.db_table,
-                    Comment._meta.pk.attname,
-                    ContentType.objects.get_for_model(Comment).id
-                )
+                Vote._meta.db_table,
+                Comment._meta.db_table,
+                Comment._meta.pk.attname,
+                ContentType.objects.get_for_model(Comment).id,
+                Vote._meta.db_table,
+                Comment._meta.db_table,
+                Comment._meta.pk.attname,
+                ContentType.objects.get_for_model(Comment).id)
             }
         )
         questions = questions.order_by('-vote_score', '-submit_date')
 
         return questions
-        
+
     def get_askmama_latest_pinned_post(self):
         """ Get the latest askmama category pinned post
         """
