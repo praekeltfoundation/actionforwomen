9883ae591155334a86467aefd28ecec8eb21907f
Avukonke Peter <avukonke@juizi.com>
2014-04-08 12:46:00 +0200

    Factored out some of the functionality from sendstagemessages into a function

--- mama/utils.py
+++ mama/utils.py
@@ -1,5 +1,15 @@
+from BeautifulSoup import BeautifulSoup
+
+
 def mobile_number_to_international(mobile_number):
     if mobile_number.startswith('0') and len(mobile_number) == 10:
             mobile_number = '27' + mobile_number[1:]
     return mobile_number
 
+
+def format_html_string(html_string):
+    #PARSE HTML INTO PLAIN TEXT
+    soup = BeautifulSoup(html_string)
+
+    clean_string = '\n'.join([e.replace("\r", "") for e in soup.recursiveChildGenerator() if isinstance(e, unicode)])
+    return clean_string
