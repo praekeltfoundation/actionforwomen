bac438b92fbb8495e2f1fdfc9344aea63d326a1f
Milton Madanda <madandat@gmail.com>
2014-05-13 15:31:10 +0200

    remove version fix

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'django-registration==0.8',
         'django-ckeditor',
         'gunicorn',
-        'django-photologue==2.7',
+        'django-photologue>2.6.praekelt',
         'jmbo==0.5.5',
         'jmbo-poll>=0.0.9',
         'jmbo-post',
