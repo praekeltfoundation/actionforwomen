d7c671c9ca42ae6e96775a6cd8b2897580757a5a
Milton Madanda <madandat@gmail.com>
2014-05-26 12:58:37 +0200

    use requirements.pip to install photologue

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ setup(
     license='BSD',
     url='http://github.com/praekelt/mama',
     packages=find_packages(),
-    dependency_links=[
-        'https://github.com/praekelt/django-photologue/tarball/2.10.praekelt#egg=django-photologue-2.10.praekelt',
-    ],
     install_requires=[
         'django==1.4.5',
         'django-category==0.1',
@@ -34,7 +31,6 @@ setup(
         'django-registration==0.8',
         'django-ckeditor',
         'gunicorn',
-        'django-photologue==2.10.praekelt',
         'jmbo==0.5.5',
         'jmbo-poll>=0.0.9',
         'jmbo-post',
