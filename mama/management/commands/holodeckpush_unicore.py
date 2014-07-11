0c96cb59eb8e2b6e4133667321d170a9ca4b63c3
Milton Madanda <madandat@gmail.com>
2014-07-11 13:45:02 +0200

    add holodeck push for unicore

---
+++ mama/management/commands/holodeckpush_unicore.py
@@ -0,0 +1,191 @@
+from datetime import datetime, timedelta
+
+from django.core.management.base import BaseCommand
+from django.contrib.comments.models import Comment
+from google_credentials import utils
+from photon import Client
+from django.contrib.auth.models import User
+from mama.models import UserProfile
+
+GA_PROFILE_ID = 88472004
+init_date = datetime(2014, 7, 14)
+
+HOLODECK_API_KEYS = {
+    'users_weekly': '',
+    'pageviews_weekly': '',
+    'user_types': '',
+    'user_phase': '',
+    'users_cumulative': '',
+    'pageviews_cumulative': '',
+    'comments_weekly': '',
+    'comments_cumulative': ''
+}
+
+
+class Command(BaseCommand):
+    help = 'Pushes various metrics for Unicore to Holodeck dashboard.'
+
+    def handle(self, *args, **options):
+        self.push(datetime.now())
+        self.push_cumulative(datetime.now())
+        print "Done!"
+
+    def push(self, datetime_obj):
+        range_end = datetime_obj
+        range_start = range_end - timedelta(days=7)
+
+        # Don't use stats older than Unicore launch date
+        if datetime_obj < init_date:
+            return
+
+        client = Client(server='http://holodeck.praekelt.com')
+        ga_service = utils.get_service()
+
+        unicore_users = User.objects.filter(userprofile__origin='unicore')
+
+        print "Pushing Unicore Weekly Users"
+        query = ga_service.data().ga().get(
+            ids='ga:%d' % GA_PROFILE_ID,
+            start_date=str(range_start.date()),
+            end_date=str(range_end.date()),
+            segment='gaid::-11',  # mobile users only
+            metrics='ga:users'
+        )
+
+        results = query.execute()
+        client.send(
+            samples=(
+                ("GA Users", results['totalsForAllResults']['ga:users']),
+                ("Registration", unicore_users.filter(
+                    date_joined__range=(range_start, range_end)).count()),
+            ),
+            api_key=HOLODECK_API_KEYS['users_weekly'],
+            timestamp=datetime_obj,
+        )
+
+        print "Pushing Unicore Weekly Pageviews"
+        query = ga_service.data().ga().get(
+            ids='ga:%d' % GA_PROFILE_ID,
+            start_date=str(range_start.date()),
+            end_date=str(range_end.date()),
+            segment='gaid::-11',  # mobile users only
+            metrics='ga:pageviews'
+        )
+
+        results = query.execute()
+        client.send(
+            samples=(
+                ("Pageviews", results['totalsForAllResults']['ga:pageviews']),
+            ),
+            api_key=HOLODECK_API_KEYS['pageviews_weekly'],
+            timestamp=datetime_obj,
+        )
+
+        print "Pushing Unicore User Types"
+        query = ga_service.data().ga().get(
+            ids='ga:%d' % GA_PROFILE_ID,
+            start_date=str(range_start.date()),
+            end_date=str(range_end.date()),
+            metrics='ga:users',
+            dimensions='ga:userType',
+            segment='gaid::-11',  # mobile users only
+        )
+
+        results = query.execute()
+        if 'rows' in results:
+            client.send(
+                samples=[(row[0], int(row[1])) for row in results['rows']],
+                api_key=HOLODECK_API_KEYS['user_types'],
+                timestamp=datetime_obj,
+            )
+
+        print "Pushing Unicore User Phase"
+        unicore_userprofile = UserProfile.objects.filter(origin='unicore')
+        client.send(
+            samples=(
+                ("Prenatal", unicore_userprofile.filter(
+                    delivery_date__gte=datetime_obj).count()),
+                ("Postnatal", unicore_userprofile.filter(
+                    delivery_date__lt=datetime_obj).count()),
+            ),
+            api_key=HOLODECK_API_KEYS['user_phase'],
+            timestamp=datetime_obj,
+        )
+
+        print "Pushing Weekly Comments"
+        unicore_comments = Comment.objects.filter(
+            user__userprofile__origin='unicore')
+
+        client.send(
+            samples=(
+                ("Unicore", unicore_comments.filter(
+                    submit_date__range=(range_start, range_end)).count()),
+            ),
+            api_key=HOLODECK_API_KEYS['comments_weekly'],
+            timestamp=datetime_obj,
+        )
+
+    def push_cumulative(self, datetime_obj):
+        # Don't use stats older than Unicore launch date
+        if datetime_obj < init_date:
+            return
+
+        client = Client(server='http://holodeck.praekelt.com')
+        ga_service = utils.get_service()
+
+        range_end = datetime_obj
+        range_start_cumulative = init_date
+        unicore_users = User.objects.filter(userprofile__origin='unicore')
+
+        print "Pushing Unicore Users Cumulative"
+        query = ga_service.data().ga().get(
+            ids='ga:%d' % GA_PROFILE_ID,
+            start_date=str(range_start_cumulative.date()),
+            end_date=str(range_end.date()),
+            metrics='ga:users',
+            segment='gaid::-11',  # mobile users only
+        )
+
+        results = query.execute()
+        client.send(
+            samples=(
+                ("GA Users", results['totalsForAllResults']['ga:users']),
+                ("Registration", unicore_users.filter(
+                    date_joined__range=(range_start_cumulative,
+                                        range_end)).count()),
+            ),
+            api_key=HOLODECK_API_KEYS['users_cumulative'],
+            timestamp=datetime_obj,
+        )
+
+        print "Pushing Unicore Pageviews Cumulative"
+        query = ga_service.data().ga().get(
+            ids='ga:%d' % GA_PROFILE_ID,
+            start_date=str(range_start_cumulative.date()),
+            end_date=str(range_end.date()),
+            metrics='ga:pageviews',
+            segment='gaid::-11',  # mobile users only
+        )
+
+        results = query.execute()
+        client.send(
+            samples=(
+                ("Pageviews", results['totalsForAllResults']['ga:pageviews']),
+            ),
+            api_key=HOLODECK_API_KEYS['pageviews_cumulative'],
+            timestamp=datetime_obj,
+        )
+
+        print "Pushing Comments Cumulative"
+        unicore_comments = Comment.objects.filter(
+            user__userprofile__origin='unicore')
+
+        client.send(
+            samples=(
+                ("Unicore", unicore_comments.filter(
+                    submit_date__range=(range_start_cumulative,
+                                        range_end)).count()),
+            ),
+            api_key=HOLODECK_API_KEYS['comments_cumulative'],
+            timestamp=datetime_obj,
+        )
