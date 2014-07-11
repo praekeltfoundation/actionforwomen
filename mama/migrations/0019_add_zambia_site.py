6e0c420a980b12208e234e672d34fa4ed47a6b3c
Milton Madanda <madandat@gmail.com>
2014-07-11 16:13:25 +0200

    fix migration

--- mama/migrations/0019_add_zambia_site.py
+++ mama/migrations/0019_add_zambia_site.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from south.utils import datetime_utils as datetime
 from south.db import db
-from south.v2 import SchemaMigration
+from south.v2 import DataMigration
 from django.db import models
-
 
 class Migration(DataMigration):
 
@@ -204,3 +203,4 @@ class Migration(DataMigration):
     }
 
     complete_apps = ['mama']
+    symmetrical = True
