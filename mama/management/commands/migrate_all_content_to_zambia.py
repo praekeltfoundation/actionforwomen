01806cd161634d963f972c43bc0e585fdb0a7d6c
Milton Madanda <madandat@gmail.com>
2014-07-14 11:00:14 +0200

    Polls cannot be shared

--- mama/management/commands/migrate_all_content_to_zambia.py
+++ mama/management/commands/migrate_all_content_to_zambia.py
@@ -1,13 +1,18 @@
 from django.core.management.base import BaseCommand
-from jmbo.models import ModelBase
 
 
 class Command(BaseCommand):
     help = 'Imports raw Baby Center baby content.'
 
     def handle(self, *args, **options):
+        from jmbo.models import ModelBase
+        from poll.models import Poll
+
         print 'Migrate Jmbo Models to Zambia'
         self.chunked_migrate(ModelBase)
+
+        print 'Un migrate Poll for Zambia'
+        self.chunked_un_migrate(Poll)
 
     def chunked_migrate(self, model):
         i = 0
@@ -21,3 +26,17 @@ class Command(BaseCommand):
                 p.sites.add(2)
             i += chunk
             print 'Items migrated to Zambia: ', i
+
+    def chunked_un_migrate(self, model):
+        i = 0
+        chunk = 100
+        while True:
+            qs = model.objects.filter(sites=2)[:chunk]
+            if not qs.exists():
+                print 'Nothing to un-migrate.'
+                break
+            for p in qs:
+                p.sites = [1, ]
+                p.save()
+            i += chunk
+            print 'Items un-migrated to Zambia: ', i
