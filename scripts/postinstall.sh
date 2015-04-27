399630d5f7e583bcef374d145de0dfdf84023f00
Milton Madanda <madandat@gmail.com>
2015-04-27 10:34:53 +0200

    fix postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,8 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
+cd "${INSTALLDIR}/${REPO}/"
+
 $manage syncdb --noinput --migrate
 $manage collectstatic --noinput
 
-sudo supervisorctl restart actionforwomen_mobi actionforwomen_mobi_celery
+sudo supervisorctl restart actionforwomen_mobi actionforwomen_celery
