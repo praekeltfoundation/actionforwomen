5679412d2950e83ef05571e8f035463968ad15e0
Milton Madanda <madandat@gmail.com>
2015-05-19 14:47:42 +0200

    fix search

--- app/search_indexes.py
+++ app/search_indexes.py
@@ -1,9 +1,6 @@
 from haystack.indexes import CharField, SearchIndex, MultiValueField
 from haystack import site
 from post.models import Post
-from django.contrib.comments import Comment
-from moderator.models import CommentReply
-from livechat.models import LiveChat, LiveChatResponse
 from django.db.models import Q
 from itertools import chain
 
@@ -15,42 +12,3 @@ class PostIndex(SearchIndex):
         return Post.permitted.all()
 
 site.register(Post, PostIndex)
-
-
-class CommentIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
-
-    def index_queryset(self):
-        ask_mama = Post.objects.get(primary_category__slug='ask-mama')
-        livechats = LiveChat.objects.all()
-        livechatresponse = LiveChatResponse.objects.all()
-
-        return Comment.objects.filter(Q(
-            object_pk__in=[ask_app.id, ] + [chat.id for chat in livechats],
-            replied_to_comments_set__isnull=False) | Q(
-            id__in=[comment.comment_id for comment in livechatresponse]))
-
-
-site.register(Comment, CommentIndex)
-
-
-class CommentReplyIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
-
-    def index_queryset(self):
-        ask_mama = Post.objects.get(primary_category__slug='ask-mama')
-
-        return CommentReply.objects.filter(
-            replied_to_comments__object_pk__in=[ask_app.id,])
-
-site.register(CommentReply, CommentReplyIndex)
-
-
-class LiveChatResponseIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
-
-    def index_queryset(self):
-        return LiveChatResponse.objects.all()
-
-site.register(LiveChatResponse, LiveChatResponseIndex)
-
