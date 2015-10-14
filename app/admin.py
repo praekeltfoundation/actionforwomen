d99747d8df531bec51fe1f9e94188b69020e2db9
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-10-14 10:25:54 +0200

    removed user_profile variable and refer directly to user.profile

--- app/admin.py
+++ app/admin.py
@@ -418,11 +418,9 @@ class DownloadableUserAdmin (UserAdmin):
         writer.writerow(["email", "mobile_number", "display_name", "gender",
                         "year_of_birth", "identity"])
         for user in queryset:
-            user_profile = user.userprofile_set.all()[0]
-
-            writer.writerow([user.email, user_profile.mobile_number,
-                            user_profile.alias, user_profile.gender,
-                            user_profile.year_of_birth, user_profile.identity])
+            writer.writerow([user.email, user.profile.mobile_number,
+                            user.profile.alias, user.profile.gender,
+                            user.profile.year_of_birth, user.profile.identity])
 
         f.seek(0)
         response = HttpResponse(f, content_type='text/csv')
